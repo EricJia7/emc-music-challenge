b7f479ca7ab51ef1abcb68a1d17c564346607092
Felipe Vega <fvega@livevox.com>
2021-09-15 13:47:20 -0700

    Completing landing page

--- emc-payment-module/src/App.js
+++ emc-payment-module/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BgContainer, Logo, Button, Side, Title, Ad } from './AppStyles';
+import bg from './assets/bg-landing.png';
+import logo from './assets/emc-logo.png';
+import { Global, css } from '@emotion/react';
 
 function App() {
+
+  const handleGetPremiumClick = () => {
+    console.log(
+      `%c
+      < Happy coding! >
+        
+      :::      ::: :::  === :::===== :::  === :::====  :::  ===
+      :::      ::: :::  === :::      :::  === :::  === :::  ===
+      ===      === ===  === ======   ===  === ===  ===  ====== 
+      ===      ===  ======  ===       ======  ===  ===  ====== 
+      ======== ===    ==    ========    ==     ======  ===  ===
+                                                          
+    `,
+      "font-family:monospace"
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Global
+        styles={css`
+          @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;500;600;700&display=swap');
+        `}
+      />
+      <BgContainer>
+        <img src={bg} alt="background" />
+      </BgContainer>
+      <Side>
+        <Logo src={logo} />
+        <Title>Unlimited</Title>
+        <Ad>Go premium be happy</Ad>
+        <Button onClick={handleGetPremiumClick}>Get premium</Button>
+      </Side>
+    </>
   );
 }
 
